a3c77e51f5d8f2fa3e5c48d93b0be5021e4770db
harshnatuskar <harshnatuskar10@gmail.com>
2023-09-09 22:23:55 +0530

    frame for the calc complete]

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [display,setDisplay] = useState('0')
+  
+  const handleNum = (event) =>{
+    const number=event.target.textContent;
+    if(display==="0"){
+      setDisplay(number);
+    }else{
+      setDisplay(display+number);
+    }
+  }
+
+  const handleClear=(event)=>{
+    setDisplay('0');
+  }
+
+  const handleOperators = (event) =>{
+    const operators= event.target.textContent;
+
+    setDisplay(display + ' ' + operators + ' ');
+  }
+
+  const handleEqual = (event) =>{
+     setDisplay(eval(display));
+  }
+
+  const handleDecimal = (event) =>{
+    const array = display.split(' ');
+    const end =array[array.length-1];
+
+    if(!end.includes('.') && (typeof parseInt(end)==='number')){
+      setDisplay(display+ ".");
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className='calculator'> 
+        <div id="display" className='row'>
+          {display}
+        </div>
+        <button id="clear" className='row' onClick={handleClear}>AC</button>
+        
+        <button id="seven" onClick={handleNum}>7</button>
+        <button id="eight"  onClick={handleNum}>8</button>
+        <button id="nine" onClick={handleNum}>9</button>
+        <button id="multiply" onClick={handleOperators}>*</button>
+
+        <button id="four" onClick={handleNum}>4</button>
+        <button id="five" onClick={handleNum}>5</button>
+        <button id="six" onClick={handleNum}>6</button>
+        <button id="divide" onClick={handleOperators}>/</button>
+
+        <button id="one" onClick={handleNum}>1</button>
+        <button id="two" onClick={handleNum}>2</button>
+        <button id="three" onClick={handleNum}>3</button>
+        <button id="add" onClick={handleOperators}>+</button>
+
+        <button id="zero" onClick={handleNum}>0</button>
+        <button id="decimal" onClick={handleDecimal}>.</button>
+        <button id="equals" onClick={handleEqual}>=</button>
+        <button id="subtract" onClick={handleOperators}>-</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
